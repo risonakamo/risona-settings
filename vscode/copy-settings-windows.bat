ffb1cf0219b911726957bb14f932af0aafee2ea5
risonakamo <hmykanae@gmail.com>
2023-07-11 23:42:45 -0400

    script to copy settings keybind update

---
+++ vscode/copy-settings-windows.bat
@@ -0,0 +1,5 @@
+set HERE=%~dp0
+cd %appdata%\Code\User
+cp settings.json keybindings.json %HERE%
+
+cd %HERE%
---
+++ vscode/override-settings-windows.bat
@@ -0,0 +1,5 @@
+@REM THIS WILL OVERRIDE YOUR SETTINGS!!!
+
+set HERE=%~dp0
+cd %HERE%
+cp settings.json keybindings.json %appdata%\Code\User
